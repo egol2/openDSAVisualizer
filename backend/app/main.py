be7af47cbccec7406180833d5a5387b367bad82b
Arjun Vellanki <arjunv905@gmail.com>
2023-09-23 15:11:50 -0400

    Initial upload file feature

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from typing import Annotated
 
 app = FastAPI()
 
@@ -6,3 +7,8 @@ app = FastAPI()
 @app.get("/")
 def hello_world():
     return {"message": "OK"}
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    open_file = await file.read()
+    return {"filename": file.filename, "items": open_file}

90f2e99dda6f795f66ceb9012dce427924cbe79b
tuanqchau <tuanqchau2810@gmail.com>
2023-09-26 22:44:02 -0400

    update stategraph

--- frontend/src/components/StateGraph.js
+++ frontend/src/components/StateGraph.js
@@ -6,30 +6,30 @@ const StateGraph = (props) => {
     return (
         <svg width="600" height="400" xmlns="http://www.w3.org/2000/svg">
             
-            <path d="M 50 200 q 250 -350 500 0" stroke="black"
+            <path d="M 50 200 q 250 -350 500 0" stroke="#454747"
             stroke-width={props.fakeFrequency[0]} fill="none" />
 
-            <path d="M 50 200 q 125 -150 250 0" stroke="black"
+            <path d="M 50 200 q 125 -150 250 0" stroke="#454747"
             stroke-width={props.fakeFrequency[1]} fill="none" />
 
-            <path d="M 300 200 q 125 -150 250 0" stroke="black"
+            <path d="M 300 200 q 125 -150 250 0" stroke="#454747"
             stroke-width={props.fakeFrequency[2]} fill="none" />
 
-            <path d="M 50 200 q 125 150 250 0" stroke="black"
+            <path d="M 50 200 q 125 150 250 0" stroke="#454747"
             stroke-width={props.fakeFrequency[3]} fill="none" />
 
-            <path d="M 300 200 q 125 150 250 0" stroke="black"
+            <path d="M 300 200 q 125 150 250 0" stroke="#454747"
             stroke-width={props.fakeFrequency[4]} fill="none" />
 
-            <path d="M 50 200 q 250 350 500 0" stroke="black"
+            <path d="M 50 200 q 250 350 500 0" stroke="#454747"
             stroke-width={props.fakeFrequency[5]} fill="none" />
 
             <circle className="reading-circle" cx="50" cy="200" r="40" stroke="black" stroke-width="0" fill="#E3B448" />
             <text x="22" y="205" fill="black" font-size="15" font-weight="bold">Reading</text>
             <circle className="vis-circle" cx="300" cy="200" r="40" stroke="black" stroke-width="0" fill="#CBD18F" />
-            <text x="263" y="205" fill="black" font-size="15" font-weight="bold">Visualizing</text>
+            <text x="262" y="205" fill="black" font-size="15" font-weight="bold">Visualizing</text>
             <circle className="ex-circle" cx="550" cy="200" r="40" stroke="black" stroke-width="0" fill="#4e9147" />
-            <text x="523" y="205" fill="black" font-size="15" font-weight="bold">Exercise</text>
+            <text x="522" y="205" fill="black" font-size="15" font-weight="bold">Exercise</text>
         </svg>
     );
     

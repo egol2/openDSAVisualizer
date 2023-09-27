bccc182f3cde3e9bfe72728fcd5e1167f7c0d7e6
tuanqchau <tuanqchau2810@gmail.com>
2023-09-26 22:32:04 -0400

    scatter plot update

--- frontend/src/components/ScatterPlot.js
+++ frontend/src/components/ScatterPlot.js
@@ -45,13 +45,17 @@ const ScatterPlot = () => {
         
         //setting up axis labaling
         svg.append('text')
-            .attr('x', w/2)
+            .attr('x', w/2 - 10)
             .attr('y', h + 50)
-            .text('x');
+            .text('Hints')
+            .style('font-weight', 'bold');
         svg.append('text')
-            .attr('y', h/2)
-            .attr('x', -50)
-            .text('y');
+            .attr('y', -50)
+            .attr('x', -230)
+            .text('Attemps')
+            .style('font-weight', 'bold')
+            .attr('transform', 'rotate(-90)') // Rotate the text 90 degrees counterclockwise
+            //.attr('dy', '-2em'); // Adjust the vertical positioning of the rotated label
         //setting up svg data
         svg.selectAll()
             .data(data)
@@ -59,8 +63,8 @@ const ScatterPlot = () => {
             .append('circle')
                 .attr('cx', d => xScale(d[0]))
                 .attr('cy', d => yScale(d[1]))
-                .attr('r', 2);
-
+                .attr('r', 5)
+                .style('fill', '#0069c0');
         svg.style('margin-left', '100px');
         svg.style('margin-top', '40px');
     }, [data])

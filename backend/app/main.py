6de0130364c95b0977d4f9219fd014a56f5a7ca3
tuanqchau <tuanqchau2810@gmail.com>
2023-09-28 20:11:32 -0400

    multiple files upload

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,6 +13,7 @@ origins = [
     "http://localhost",
     "http://localhost:8000",
     "http://localhost:80",
+    "http://localhost:3000"
 ]
 
 app.add_middleware(
--- frontend/src/pages/Upload.js
+++ frontend/src/pages/Upload.js
@@ -5,66 +5,68 @@ import { TextField } from '@mui/material';
 import FileUploadIcon from '@mui/icons-material/FileUpload';
 import Box from '@mui/material/Box';
 import { Navigate } from 'react-router-dom';
-
 import CloseIcon from '@mui/icons-material/Close';
 import IconButton from '@mui/material/IconButton';
-
 import '../styles/Upload.css';
-
 import { Outlet, Link } from "react-router-dom";
 
 const Upload = () => {
-    const [selectedFile, setSelectedFile] = useState(null);
-    const [selectedScoreFile, setSelectedScoreFile] = useState(null);
-    const [selectedInteractionFile, setSelectedInteractionFile] = useState(null);
+    const [exFile, setExFile] = useState(null);
+    const [intFile, setIntFile] = useState(null);
+    const [scoreFile, setScoreFile] = useState(null);
 
-    const [navigateToDashboard, setNavigateToDashboard] = useState(false);
-    
-    if (navigateToDashboard) {
-      return <Navigate to="/dashboard" />;
-    }
 
-    const handleFileChange = (event) => {
-      const exerciseFile = event.target.files[0];
-      // const interactionFile = event.target.files[0];
-      // const scoreFile = event.target.files[0];
-      setSelectedFile(exerciseFile);
-      // setSelectedScoreFile(interactionFile);
-      // setSelectedInteractionFile(scoreFile);
-
-   
+    const handleFileChange = (event, setter) => {
+      const file = event.target.files[0];
+      setter(file);
     };
-  
-    const handleUpload = async () => {
+
+    const sendFileToEndpoint = async (file, endpoint) => {
+        if (file) {
+            const formData = new FormData();
+            formData.append('file', file);
+
+            try {
+                const response = await fetch(`http://localhost:8000${endpoint}`, { // '/upload-endpoint' is your server's endpoint to handle file uploads
+                    method: 'POST',
+                    body: formData,
+                });
+        
+                if (response.ok) {
+                    console.log('File uploaded successfully');
+                } else {
+                    console.error('File upload failed:', await response.text());
+                }
+                
+                if (endpoint === '/upload/scores') {
+                    const res = await fetch('http://localhost:8000/scores', {
+                        method: 'GET',
+                    });
+                }
+                
+
+            } catch (error) {
+                console.error('Error uploading the file:', error);
+            }
+        }
+    };
+    const handleUpload = () => {
       // Handle file upload logic here, e.g., send the file to a server.
-      if (selectedFile) {
-        console.log('Uploading file:', selectedFile);
-        // console.log('Uploading file:', selectedScoreFile);
-        // console.log('Uploading file:', selectedInteractionFile);
-        //sending the file to a server
+        console.log('Uploading file:', exFile);
+        console.log('Uploading file:', intFile);
+        console.log('Uploading file:', scoreFile);
+        //event.preventDefault();
+
         const formData = new FormData();
 
-        formData.append('file', selectedFile);  // 'file' is the field name for the server to retrieve the file
+        formData.append('exfile', exFile);  // 'file' is the field name for the server to retrieve the file
+        formData.append('intfile', intFile);
+        formData.append('scorefile', scoreFile);
 
         // Send the file to the server
-        try {
-            const response = await fetch('http://localhost:8000/uploadfile', { // '/upload-endpoint' is your server's endpoint to handle file uploads
-                method: 'POST',
-                body: formData,
-            });
-    
-            if (response.ok) {
-                console.log('File uploaded successfully');
-            } else {
-                console.error('File upload failed:', await response.text());
-            }
-        } catch (error) {
-            console.error('Error uploading the file:', error);
-        }
-      }
-      //setNavigateToDashboard(true);
-
-      
+        sendFileToEndpoint(formData.get('exfile'), '/upload/exercises');
+        sendFileToEndpoint(formData.get('intfile'), '/upload/interactions');
+        sendFileToEndpoint(formData.get('scorefile'), '/upload/scores');
     };
   
     return (
@@ -77,26 +79,30 @@ const Upload = () => {
             </Link>
             
             <div className='input-file-wrapper'>
-                <Box className='input-box' sx={{ display: 'flex', alignItems: 'center' }}>
-                    <FileUploadIcon sx={{ color: 'action.active', mr: 0.5, my: 0.5 }} />
-                    <TextField label="Exercises"
-                    InputLabelProps={{shrink: true,}} type="file" className="input-box" onChange={handleFileChange} />
-                </Box>
-                <Box className='input-box' sx={{ display: 'flex', alignItems: 'center' }}>
-                    <FileUploadIcon sx={{ color: 'action.active', mr: 0.5, my: 0.5 }} />
-                    <TextField label="Interactions"
-                    InputLabelProps={{shrink: true,}} type="file" className="input-box" onChange={handleFileChange} />
-                </Box>
-                <Box className='input-box' sx={{ display: 'flex', alignItems: 'center' }}>
-                    <FileUploadIcon sx={{ color: 'action.active', mr: 0.5, my: 0.5 }} />
-                    <TextField label="Scores"
-                    InputLabelProps={{shrink: true,}} type="file" className="input-box" onChange={handleFileChange} />
-                </Box>
-                <Link to="/Dashboard">
+                
+                    <Box className='input-box' sx={{ display: 'flex', alignItems: 'center' }}>
+                        <FileUploadIcon sx={{ color: 'action.active', mr: 0.5, my: 0.5 }} />
+                        <TextField label="Exercises"
+                        InputLabelProps={{shrink: true,}} type="file" className="input-box" onChange={(event) => handleFileChange(event, setExFile)} />
+                    </Box>
+                    <Box className='input-box' sx={{ display: 'flex', alignItems: 'center' }}>
+                        <FileUploadIcon sx={{ color: 'action.active', mr: 0.5, my: 0.5 }} />
+                        <TextField label="Interactions"
+                        InputLabelProps={{shrink: true,}} type="file" className="input-box" onChange={(event) => handleFileChange(event, setIntFile)} />
+                    </Box>
+                    <Box className='input-box' sx={{ display: 'flex', alignItems: 'center' }}>
+                        <FileUploadIcon sx={{ color: 'action.active', mr: 0.5, my: 0.5 }} />
+                        <TextField label="Scores"
+                        InputLabelProps={{shrink: true,}} type="file" className="input-box" onChange={(event) => handleFileChange(event, setScoreFile)} />
+                    </Box>
+                    {/* <Link to="/Dashboard">
+                        
+                    </Link> */}
                     <Button className='upload-button' size="large" variant="outlined" color="error" onClick={handleUpload}>
-                    Upload
+                        Upload
                     </Button>
-                </Link>
+                
+                
             </div>        
         </div>
     );

e956fdbfc9071549598df28b49f0a6c6cfc3c523
Arjun Vellanki <arjunv905@gmail.com>
2023-09-29 21:08:26 -0400

    Complete API endpoints to interface with

--- backend/app/files.py
+++ backend/app/files.py
@@ -3,14 +3,44 @@ from io import BytesIO
 import os
 
 # Takes in the byte buffer of the file contents as a .tsv and splits the contents of the file into separate .log files for each student
-def segment(contents):
-    dataset = pd.read_csv(BytesIO(contents), delimiter='\t')
+def segment(contents, type):
+    ext = ""
+    delim = ""
+
+    if type == "interactions":
+        ext = ".log"
+        delim = '\t'
+    elif type == "exercises":
+        ext = ".exercises"
+        delim = ","
+    
+    dataset = pd.read_csv(BytesIO(contents), delimiter=delim)
     dataset = pd.DataFrame(dataset)
     ids = dataset['user_id'].unique()
 
-    if not os.path.isdir("/app/app/data/"):
-        os.mkdir("/app/app/data/")
-
     for id_idx in range(len(ids)):
-        data_slice = dataset[dataset['user_id'] == ids[id_idx]].set_index('id', drop=True)
-        data_slice.to_csv("/app/app/data/" + ids[id_idx].astype(str) + '.log')
+        data_slice = dataset[dataset['user_id'] == ids[id_idx]]
+        data_slice.to_csv("/app/app/data/" + ids[id_idx].astype(str) + ext, index=False)
+
+def handle_scores(contents):
+    dataset = pd.read_csv(BytesIO(contents))
+    dataset = pd.DataFrame(dataset)
+    ids = dataset['user_id'].unique()
+
+    # Building an array of json objects, where each object contains information regarding one student
+    result = []
+
+    for idx in dataset.index:
+        result.append({
+            "user_id": dataset['user_id'][idx],
+            "Projects": dataset['Projects / 100'][idx],
+            "Midterm": dataset['Midterm / 100'][idx],
+            "Final": dataset['Final / 100'][idx],
+            "OpenDSA": dataset['OpenDSA Exercises / 100'][idx],
+            "Total": dataset['Final Score / 100'][idx],
+            "Grade": dataset['Letter Grade'][idx],
+        })
+
+    json_file = open("/app/app/data/.scores", "w")
+    json_file.write(str(result))
+    json_file.close()
--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, File, UploadFile, HTTPException
 from typing import Annotated
 from fastapi.middleware.cors import CORSMiddleware
 import os
+import glob
 
 from . import abstractor, files, behaviors
 
@@ -23,19 +24,23 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-root_path = "/app/app/"
-
-@app.get("/")
-def hello_world():
-    return {"message": "OK"}
 
 @app.post("/upload/scores")
 async def upload_scores(file: UploadFile):
-    open_file = await file.read()
+    # Reading the uploaded file to ensure valid format
+    try:
+        file_contents = await file.read()
+    except:
+        raise HTTPException(status_code=422, detail="Unable to process file")
     
-    # Check if the ./data folder exists or has contents, if it does delete them (ONLY .scores FILES!)
+    # Check if the ./data folder has contents, if it does delete them (only the .scores file)
+    if os.path.isfile("/app/app/data/.scores"):
+        os.remove("/app/app/data/.scores")
 
-    return {"filename": file.filename, "items": open_file}
+    # Parse and store the scores file in a json format
+    files.handle_scores(file_contents)
+    
+    return
 
 @app.post("/upload/interactions")
 async def upload_interactions(file: UploadFile):
@@ -45,46 +50,76 @@ async def upload_interactions(file: UploadFile):
     except:
         raise HTTPException(status_code=422, detail="Unable to process file")
     
-    # Check if the ./data folder exists or has contents, if it does delete them (ONLY .log AND .json FILES!)
+    # Check if the ./data folder has contents, if it does delete them (only .log and .json files)
+    if os.path.isdir("/app/app/data"):
+        for log in glob.glob("/app/app/data/*.log"):
+            os.remove(log)
+        for json in glob.glob("/app/app/data/*.json"):
+            os.remove(json)
 
     # Segmenting the file contents into separate .log files based on student IDs
-    files.segment(file_contents)
+    files.segment(file_contents, "interactions")
 
-    return {"filename": file.filename}
+    return
 
 @app.post("/upload/exercises")
 async def upload_exercises(file: UploadFile):
-    open_file = await file.read()
+    # Reading the uploaded file to ensure valid format
+    try:
+        file_contents = await file.read()
+    except:
+        raise HTTPException(status_code=422, detail="Unable to process file")
     
-    # Check if the ./data folder exists or has contents, if it does delete them (ONLY .exercises FILES!)
+    # Check if the ./data folder has contents, if it does delete them (only .exercises files)
+    if os.path.isdir("/app/app/data"):
+        for exercise in glob.glob("/app/app/data/*.exercises"):
+            os.remove(exercise)
 
-    return {"filename": file.filename, "items": open_file}
+    # Segmenting the file contents into separate .exercises 
+    files.segment(file_contents, "exercises")
+
+    return 
+
+@app.get("/scores")
+async def scores():
+    # Check if the .scores file exists, and send it
+    if os.path.isfile("/app/app/data/.scores"):
+        file = open("/app/app/data/.scores", "r")
+        json = file.read()
+        return json
+    else:
+        raise HTTPException(status_code=404, detail="Scores not found")
 
 @app.get("/student/{id}")
 async def student_info(id):
     # Check if .json file of student exists and return it
-    full_json_path = root_path + "data/" + str(id) + ".json"
+    full_json_path = "/app/app/data/" + str(id) + ".json"
     if os.path.isfile(full_json_path):
         json_file = open(full_json_path, "r")
-        json = json_file.readlines()
+        json = json_file.read()
         return json
 
     # Check if .log file of student exists and process
-    full_item_path = root_path + "data/" + str(id) + ".log"
+    full_item_path = "/app/app/data/" + str(id) + ".log"
     if not os.path.isfile(full_item_path):
         raise HTTPException(status_code=404, detail="Student ID not found")
     
+    # Collecting the abstracted event data
     data = abstractor.abstract(full_item_path)
         
-    #valA = behaviors.a(data)
-    #valB = behaviors.b(data)
+    # Collecting student behavior info
     (total_transitions, transitions) = behaviors.getTransitionCounts(data)
     
-    # Writing the resultant values into a .json
+    # Compiling the resultant values
     result = {
         "id": id,
         "total_transitions": total_transitions,
         "transitions": transitions
     }
+
+    # Writing the .json
+    json_file = open(full_json_path, "w")
+    json_file.write(str(result))
+    json_file.close()
     
     return result

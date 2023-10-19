66718c53951e4809273e4755ff51176655cadd59
egor <egoriluk@gmail.com>
2023-10-19 18:37:39 -0400

    Tuans arrows, I give up

--- frontend/src/components/StateGraph.js
+++ frontend/src/components/StateGraph.js
@@ -20,43 +20,42 @@ const StateGraph = (props) => {
                 
             <svg width="600" height="400" xmlns="http://www.w3.org/2000/svg">
 
-                <defs>
-                    <marker id="arrowhead" markerWidth="3.34" markerHeight="5" refX="5.5" refY="2.5" 
-                            orient="auto" overflow="visible">
-                        <path d="M0 0 L3.34 2.5 L0 5" stroke="#454747" fill="none" stroke-width=".6" />
-                    </marker>
+            <path d="M 50 200 q 250 -350 500 0" stroke="#454747"
+                strokeWidth={normalize(props.frequency.transitions.RE)} fill="none">
+                <title>Reading → Exercise: {props.frequency.transitions.RE}</title>
+            </path>
+            <polygon points="300,10 320,25 300,40" style={{fill: "#454747"}} />
 
-                    <marker id="reversedArrowhead" markerWidth="3.34" markerHeight="5" refX="-2" refY="2.5" 
-                            orient="auto" overflow="visible">
-                        <path d="M3.34 0 L0 2.5 L3.34 5" stroke="#454747" fill="none" stroke-width=".6" />
-                    </marker>
-                </defs>
+            <path d="M 50 200 q 125 -150 250 0" stroke="#454747"
+                strokeWidth={normalize(props.frequency.transitions.RV)} fill="none">
+                <title>Reading → Visualizing: {props.frequency.transitions.RV}</title>
+            </path>
+            <polygon points="170,110 190,125 170,140" style={{fill: "#454747"}} />
 
-                <path d="M 50 200 q 250 -350 500 0" stroke="#454747"
-                    strokeWidth={normalize(props.frequency.transitions.RE)} fill="none"
-                    marker-end="url(#arrowhead)">
-                    <title>Reading → Exercise: {props.frequency.transitions.RE}</title></path>
-                    
-                <path d="M 50 200 q 125 -150 250 0" stroke="#454747"
-                    strokeWidth={normalize(props.frequency.transitions.RV)} fill="none" 
-                    marker-end="url(#arrowhead)">
-                    <title>Reading → Visualizing: {props.frequency.transitions.RV}</title></path>
+            <path d="M 300 200 q 125 -150 250 0" stroke="#454747"
+                strokeWidth={normalize(props.frequency.transitions.VE)} fill="none">
+                <title>Visualizing → Exercise: {props.frequency.transitions.VE}</title>
+            </path>
+            <polygon points="420,110 450,125 420,140" style={{fill: "#454747"}} />
 
-                <path d="M 300 200 q 125 -150 250 0" stroke="#454747"
-                strokeWidth={normalize(props.frequency.transitions.VE)} fill="none"
-                marker-end="url(#arrowhead)" ><title>Visualizing → Exercise: {props.frequency.transitions.VE}</title></path>
+            <path d="M 50 200 q 125 150 250 0" stroke="#454747"
+                strokeWidth={normalize(props.frequency.transitions.VR)} fill="none">
+                <title>Visualizing → Reading: {props.frequency.transitions.VR}</title>
+            </path>
+            <polygon points="190,260 170,275 190,290" style={{fill: "#454747"}} />
 
-                <path d="M 50 200 q 125 150 250 0" stroke="#454747"
-                strokeWidth={normalize(props.frequency.transitions.VR)} fill="none" 
-                marker-start="url(#reversedArrowhead)"><title>Visualizing → Reading: {props.frequency.transitions.VR}</title></path>
+            <path d="M 300 200 q 125 150 250 0" stroke="#454747"
+                strokeWidth={normalize(props.frequency.transitions.EV)} fill="none">
+                <title>Exercise → Visualizing: {props.frequency.transitions.EV}</title>
+            </path>
+            <polygon points="450,260 420,275 450,290" style={{fill: "#454747"}} />
 
-                <path d="M 300 200 q 125 150 250 0" stroke="#454747"
-                strokeWidth={normalize(props.frequency.transitions.EV)} fill="none" 
-                marker-start="url(#reversedArrowhead)"><title>Exercise → Visualizing: {props.frequency.transitions.EV}</title></path>
+            <path d="M 50 200 q 250 350 500 0" stroke="#454747"
+                strokeWidth={normalize(props.frequency.transitions.ER)} fill="none">
+                <title>Exercise → Reading: {props.frequency.transitions.ER}</title>
+            </path>
+            <polygon points="320,360 300,375 320,390" style={{fill: "#454747"}} />
 
-                <path d="M 50 200 q 250 350 500 0" stroke="#454747"
-                strokeWidth={normalize(props.frequency.transitions.ER)} fill="none" 
-                marker-start="url(#reversedArrowhead)"><title>Exercise → Reading: {props.frequency.transitions.ER}</title></path>
 
                 <circle className="reading-circle" cx="50" cy="200" r="40" stroke="black" strokeWidth="0" fill="#E3B448" />
                 <text x="22" y="205" fill="black" fontSize="15" fontWeight="bold">Reading</text>
